672969519bf0f37835e9ee8010111587ea26116b
ricardo-soares-plexit <ricardo.soares@plexit.pt>
2026-08-25 15:33:00 +0100

    chore: add more shadcn components

---
+++ components/ui/spinner.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+import { LoaderCircleIcon } from 'lucide-react';
+
+function Spinner({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <LoaderCircleIcon
+      data-slot="spinner"
+      role="status"
+      aria-label="Loading"
+      className={cn('size-4 animate-spin', className)}
+      {...props}
+    />
+  );
+}
+
+export { Spinner };
