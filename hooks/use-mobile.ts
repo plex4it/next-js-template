d506bd555d0d9904ff76c6a9cddcbd93dbb63bf9
ricardo-soares-plexit <ricardo.soares@plexit.pt>
2026-08-27 11:26:29 +0100

    feat: refactor useIsMobile hook to use React's useSyncExternalStore for better performance

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,20 +1,19 @@
+'use client';
+
 import * as React from 'react';
 
-const MOBILE_BREAKPOINT = 768;
-
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+export function useIsMobile(breakpoint: number = 1024) {
+  const query = `(max-width: ${breakpoint - 1}px)`;
+  const { subscribe, getSnapshot } = React.useMemo(() => {
+    return {
+      subscribe: (onStoreChange: () => void) => {
+        const mql = window.matchMedia(query);
+        mql.addEventListener('change', onStoreChange);
+        return () => mql.removeEventListener('change', onStoreChange);
+      },
+      getSnapshot: () => window.matchMedia(query).matches,
     };
-    mql.addEventListener('change', onChange);
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener('change', onChange);
-  }, []);
+  }, [query]);
 
-  return !!isMobile;
+  return React.useSyncExternalStore(subscribe, getSnapshot, () => false);
 }
